cd39de5f141e7904b1348af179b6dc5c9f3066f0
ElizabethHauroo <eliza-hauroo13@hotmail.com>
2023-02-28 12:26:34 +0000

    deploy file commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+#Install pm2
+sudo npm install -g pm2
+#stop any instance currently running 
+pm2 stop example_app
+#change directory
+cd ExampleApplication
+#install dependencies
+npm install
+#start the app
+pm2 start ./bin/www --name example_app
